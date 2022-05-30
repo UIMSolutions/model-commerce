30d286f35f06aeb9fbdfb9a5d87eddcee30d9c78
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-05-30 10:04:29 +0200

    Adding entities for cash management

--- source/models/commerce/entities/cash/package.d
+++ source/models/commerce/entities/cash/package.d
@@ -2,3 +2,6 @@ module models.commerce.entities.cash;
 
 @safe:
 import models.commerce;
+
+public import models.commerce.entities.cash.retailposbatch;
+public import models.commerce.entities.cash.retailposbatch2;
